8d9c2123597f364492b8723f6cc9211aade32312
kode2go <brsbin001@myuct.ac.za>
2022-09-24 08:31:05 +0200

    Update myst01.py

--- myst01.py
+++ myst01.py
@@ -6,6 +6,25 @@ st.write(""" # Hello World
 
 My first app """)
 
+import plotly.figure_factory as ff
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
 
