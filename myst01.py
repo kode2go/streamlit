5a69dd4c7a34985ec94643fe16128ff6a9021846
kode2go <brsbin001@myuct.ac.za>
2022-09-24 08:25:58 +0200

    Update myst01.py

--- myst01.py
+++ myst01.py
@@ -37,3 +37,16 @@ option = st.selectbox(
      df['first column'])
 
 'You selected: ', option
+
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
