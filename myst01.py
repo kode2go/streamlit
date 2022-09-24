86b28ab55d270ba3463ef153bbd383d82c02fe24
kode2go <brsbin001@myuct.ac.za>
2022-09-24 08:24:56 +0200

    Update myst01.py

--- myst01.py
+++ myst01.py
@@ -1,4 +1,39 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.write("""Hello World 
+
 My first app """)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+    
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
