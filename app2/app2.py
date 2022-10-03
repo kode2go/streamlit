f45827b7873939f96d933f15d337e13e48a51e0b
kode2go <brsbin001@myuct.ac.za>
2022-10-03 14:46:51 +0200

    Update app2.py

--- app2/app2.py
+++ app2/app2.py
@@ -5,15 +5,14 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+import numpy as np
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
