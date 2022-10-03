7c18f7a830ee6ceafebb2149f79bf7278f423769
kode2go <brsbin001@myuct.ac.za>
2022-10-03 15:38:44 +0200

    Update app2.py

--- app2/app2.py
+++ app2/app2.py
@@ -7,10 +7,14 @@ import streamlit as st
 import pandas as pd
 
 st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
+
+df = pd.DataFrame(
+    {
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
-}))
+    }
+)
+st.write(df)
 
 uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
@@ -20,3 +24,10 @@ if uploaded_file is not None:
 else:
     st.warning('you need to upload a file')
     
+import numpy as np
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
