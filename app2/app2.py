dbc999b82078452df5f482e95ba777b1b054dc6e
kode2go <brsbin001@myuct.ac.za>
2022-10-03 15:46:22 +0200

    Update app2.py

--- app2/app2.py
+++ app2/app2.py
@@ -9,62 +9,62 @@ import numpy as np
 
 st.write("Here's our first attempt at using data to create a table:")
 
-# df = pd.DataFrame(
-#     {
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40]
-#     }
-# )
-# st.write(df)
+df = pd.DataFrame(
+    {
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    }
+)
+st.write(df)
 
-# uploaded_file = st.file_uploader('Choose a file')
-# if uploaded_file is not None:
-#     #read csv
-#     df1=pd.read_csv(uploaded_file)
-#     st.write(df1)
-# else:
-#     st.warning('you need to upload a file')
+uploaded_file = st.file_uploader('Choose a file')
+if uploaded_file is not None:
+    #read csv
+    df1=pd.read_csv(uploaded_file)
+    st.write(df1)
+else:
+    st.warning('you need to upload a file')
     
 
-# chart_data = pd.DataFrame(
-#      np.random.randn(20, 3),
-#      columns=['a', 'b', 'c'])
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
 
-# st.line_chart(chart_data)
+st.line_chart(chart_data)
 
-# x = st.slider('x')  # 👈 this is a widget
-# st.write(x, 'squared is', x * x)
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
 
-# df = pd.DataFrame({
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40]
-#     })
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
 
-# option = st.selectbox(
-#     'Which number do you like best?',
-#      df['first column'])
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
 
-# 'You selected: ', option
+'You selected: ', option
 
-# # Add a selectbox to the sidebar:
-# add_selectbox = st.sidebar.selectbox(
-#     'How would you like to be contacted?',
-#     ('Email', 'Home phone', 'Mobile phone')
-# )
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
 
-# # Add a slider to the sidebar:
-# add_slider = st.sidebar.slider(
-#     'Select a range of values',
-#     0.0, 100.0, (25.0, 75.0)
-# )
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
 
-left_column, right_column = st.columns(2)
-# You can use a column just like st.sidebar:
-left_column.button('Press me!')
+# left_column, right_column = st.columns(3)
+# # You can use a column just like st.sidebar:
+# left_column.button('Press me!')
 
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
+# # Or even better, call Streamlit functions inside a "with" block:
+# with right_column:
+#     chosen = st.radio(
+#         'Sorting hat',
+#         ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+#     st.write(f"You are in {chosen} house!")
