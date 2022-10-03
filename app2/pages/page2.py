7077c00892d2cf5550f5fd81f2c8f10524597072
kode2go <brsbin001@myuct.ac.za>
2022-10-03 16:03:57 +0200

    Update page2.py

--- app2/pages/page2.py
+++ app2/pages/page2.py
@@ -6,6 +6,13 @@ st.markdown("# Page 2 ❄️")
 st.sidebar.markdown("# Page 2 ❄️")
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+# col1.metric("Temperature", "70 °F", "1.2 °F")
+# col2.metric("Wind", "9 mph", "-8%")
+# col3.metric("Humidity", "86%", "4%")
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+col1.st.line_chart(chart_data)
+col2.st.line_chart(chart_data)
+col3.st.line_chart(chart_data)
