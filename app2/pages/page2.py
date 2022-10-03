2a02d896445ccf80ec7b916211ae51476ab64d9c
kode2go <brsbin001@myuct.ac.za>
2022-10-03 16:00:45 +0200

    Update page2.py

--- app2/pages/page2.py
+++ app2/pages/page2.py
@@ -5,17 +5,7 @@ import numpy as np
 st.markdown("# Page 2 ❄️")
 st.sidebar.markdown("# Page 2 ❄️")
 
-col1, col2, col3 = st.columns([3, 1])
-data = np.random.randn(10, 1)
-
-col1.subheader("A wide column with a chart")
-col1.line_chart(data)
-
-col2.subheader("A narrow column with the data")
-col2.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-
-chart_data = pd.DataFrame(
-    np.random.randn(50, 3),
-    columns=["a", "b", "c"])
-
-col3.bar_chart(chart_data)
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
