a7922d94de1b9f77a4772e4c3e77ee43eaa2498c
kode2go <brsbin001@myuct.ac.za>
2022-10-03 15:44:09 +0200

    Update app2.py

--- app2/app2.py
+++ app2/app2.py
@@ -9,42 +9,42 @@ import numpy as np
 
 st.write("Here's our first attempt at using data to create a table:")
 
-df = pd.DataFrame(
-    {
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    }
-)
-st.write(df)
+# df = pd.DataFrame(
+#     {
+#     'first column': [1, 2, 3, 4],
+#     'second column': [10, 20, 30, 40]
+#     }
+# )
+# st.write(df)
 
-uploaded_file = st.file_uploader('Choose a file')
-if uploaded_file is not None:
-    #read csv
-    df1=pd.read_csv(uploaded_file)
-    st.write(df1)
-else:
-    st.warning('you need to upload a file')
+# uploaded_file = st.file_uploader('Choose a file')
+# if uploaded_file is not None:
+#     #read csv
+#     df1=pd.read_csv(uploaded_file)
+#     st.write(df1)
+# else:
+#     st.warning('you need to upload a file')
     
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+# chart_data = pd.DataFrame(
+#      np.random.randn(20, 3),
+#      columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
+# st.line_chart(chart_data)
 
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+# x = st.slider('x')  # 👈 this is a widget
+# st.write(x, 'squared is', x * x)
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
+# df = pd.DataFrame({
+#     'first column': [1, 2, 3, 4],
+#     'second column': [10, 20, 30, 40]
+#     })
 
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
+# option = st.selectbox(
+#     'Which number do you like best?',
+#      df['first column'])
 
-'You selected: ', option
+# 'You selected: ', option
 
 # # Add a selectbox to the sidebar:
 # add_selectbox = st.sidebar.selectbox(
